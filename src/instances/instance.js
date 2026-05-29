0fca9377ae9ea37d5ab443d1e11cac9b21cf1938
chittrapriya5796 <krcpriya@gmail.com>
2026-05-29 22:55:58 +0530

    Final Update3

--- src/instances/instance.js
+++ src/instances/instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:"https://carbooking-3jga.onrender.com/api/v1",
-  timeout: 10000,
+  timeout: 60000,
   headers: {
     "Content-Type": "application/json",
   },
